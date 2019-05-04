0bb54be994852f1d2b6945229da461a52649fb9a
alice <alice@alices-Air.PK5001Z>
2019-05-04 03:52:53 -0700

    org-contacts with no errors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,21 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+        ("melpa" . "https://melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 (let ((pkgs-to-install
-       (seq-remove 'package-installed-p '(activity-watch-mode json))))
+       (seq-remove 'package-installed-p '(use-package))))
   (when pkgs-to-install
     (package-refresh-contents)
     (dolist (pkg pkgs-to-install)
       (package-install pkg))))
 
-(setq debug-on-error t)
-(require 'json)
-(global-activity-watch-mode)
+(use-package org
+  :ensure org-plus-contrib)
+
+(use-package org-contacts
+  :ensure nil
+  :after org
+  :custom (org-contacts-files '("~/.personal/agenda/contacts.org")))
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM ubuntu
-RUN apt-get update && apt-get install -y \
-        emacs \
-        && rm -rf /var/lib/apt/lists/*
+FROM nixos/nix
+
+RUN nix-env -iA nixpkgs.emacs26-nox
+
 ADD .emacs.d /root/.emacs.d
 RUN emacs -batch -l ~/.emacs.d/init.el
