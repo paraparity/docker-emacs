b79abdcd0324f019457f651b7b5705171bede15e
alice <alice@alices-Air.PK5001Z>
2019-05-10 15:48:04 -0700

    Simple init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,17 +5,7 @@
         ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
 
-(let ((pkgs-to-install
-       (seq-remove 'package-installed-p '(use-package))))
-  (when pkgs-to-install
-    (package-refresh-contents)
-    (dolist (pkg pkgs-to-install)
-      (package-install pkg))))
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(use-package org
-  :ensure org-plus-contrib)
-
-(use-package org-contacts
-  :ensure nil
-  :after org
-  :custom (org-contacts-files '("~/.personal/agenda/contacts.org")))
+(mapc 'package-install '(use-package))
